6540b01c7771f77fbd5a90b7c1668526cec3052e
HafizaFarwa <farwamahmood523@gmail.com>
2021-10-11 17:45:37 +0500

    rest api

--- tests/test_accum2.py
+++ tests/test_accum2.py
@@ -12,4 +12,6 @@ def accum():
 def test_accumlator_add_one(accum):
     accum.add(4)
     assert accum.count==4
-
+def test_accumulator_cannot_set_count_directly(accum):
+    with pytest.raises(AttributeError, match=r"can't set attribute") as e:
+     accum.count = 10
